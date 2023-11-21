00f4196660dcd0980ac5168d456676134ea488fd
HoloShaii <nijholtgerben@gmail.com>
2023-11-21 23:25:18 +0100

    Implemented Insert, Update and Delete query builders as preparation for the save/load system

--- Database/DatabaseHandler.cs
+++ Database/DatabaseHandler.cs
@@ -1,10 +1,5 @@
 ﻿using Npgsql;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Virulence.Database;
 public class DatabaseHandler : IDisposable
@@ -29,6 +24,9 @@ public class DatabaseHandler : IDisposable
     private const string _connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=3bubDr@K4W6*pvQQDCYj@@kQ;Database=Virulence;";
     private readonly NpgsqlConnection _db;
     public NpgsqlConnection DB => _db;
+
+    private int _rowsAffectedLastQuery;
+    public int RowsAffectedLastQuery => _rowsAffectedLastQuery;
 
     public DatabaseHandler()
     {
@@ -77,8 +75,8 @@ public class DatabaseHandler : IDisposable
             CommandText = query
         };
         try 
-        { 
-            command.ExecuteNonQuery();
+        {
+            _rowsAffectedLastQuery = command.ExecuteNonQuery();
             return true;
         }
         catch (Exception ex)
@@ -88,9 +86,9 @@ public class DatabaseHandler : IDisposable
         }
     }
 
-    public List<object[]> Get(string tablename, List<string> columns = null, List<(string column, string value, WhereClauseComparison compare, WhereClauseCombiner combine)> where = null, string customWhere = "")
+    public List<object[]> Get(string tableName, List<string> columns = null, List<(string column, string value, WhereClauseComparison compare, WhereClauseCombiner combine)> where = null, string customWhere = "")
     {
-        if (string.IsNullOrWhiteSpace(tablename)) return null;
+        if (string.IsNullOrWhiteSpace(tableName)) return null;
 
         columns ??= new();
         where ??= new();
@@ -99,87 +97,173 @@ public class DatabaseHandler : IDisposable
 
         if (columns.Any())
         {
-            commandText.Append(' ');
-            commandText.Append(string.Join(", ", columns));
-            commandText.Append(' ');
+            commandText.Append(' ')
+                       .Append(string.Join(", ", columns))
+                       .Append(' ');
         }
         else
         {
-            commandText.Append(" * ");
+            commandText.Append(' ')
+                       .Append('*')
+                       .Append(' ');
         }
 
-        commandText.Append($"FROM {tablename}");
+        commandText.Append("FROM")
+                   .Append(' ')
+                   .Append(tableName);
 
         if (where.Any())
         {
-            for (int i = 0; i < where.Count; i++)
-            {
-                if (i == 0) commandText.Append(" WHERE ");
-                (string column, string value, WhereClauseComparison compare, WhereClauseCombiner combine) = where[i];
-
-                commandText.Append(column);
-                commandText.Append(' ');
-
-                commandText.Append(compare switch
-                {
-                    WhereClauseComparison.Unknown => throw new NotImplementedException("Where clause cannot be Unknown!"),
-                    WhereClauseComparison.Equals => '=',
-                    WhereClauseComparison.NotEquals => "<>",
-                    WhereClauseComparison.Greater => '>',
-                    WhereClauseComparison.Less => '<',
-                    WhereClauseComparison.GreaterOrEqual => "<=",
-                    WhereClauseComparison.LessOrEqual => ">=",
-                    _ => throw new NotImplementedException("How even??"),
-                });
-
-                commandText.Append(' ');
-                commandText.Append(value);
-                commandText.Append(' ');
-
-                if (i < where.Count - 1 && combine == WhereClauseCombiner.None) combine = WhereClauseCombiner.And;
-
-                commandText.Append(combine switch
-                {
-                    WhereClauseCombiner.None => "",
-                    WhereClauseCombiner.And => "AND",
-                    WhereClauseCombiner.Or => "OR",
-                    _ => throw new NotImplementedException("How even??"),
-                });
-
-                commandText.Append(' ');
-            }
+            commandText.Append(ConstructWhere(where));
         }
         else if (!string.IsNullOrWhiteSpace(customWhere))
         {
-            commandText.Append(' ');
-            commandText.Append(customWhere);
+            commandText.Append(' ')
+                       .Append(customWhere);
         }
         commandText.Append(';');
 
-        NpgsqlCommand command = new()
-        {
-            Connection = _db,
-            CommandText = commandText.ToString()
-        };
-        try
-        {
-            NpgsqlDataReader dataReader = command.ExecuteReader();
-            List<object[]> rows = new();
-            while (dataReader.Read())
-            {
-                object[] row = new object[dataReader.FieldCount];
-                dataReader.GetValues(row);
-                rows.Add(row);
-            }
-            dataReader.Close();
+        return ExecuteQuery(commandText.ToString());
+    }
 
-            return rows;
-        }
-        catch (Exception ex)
+    public bool Insert(string tableName, List<string> columns = null, List<string> values = null)
+    {
+        if (string.IsNullOrWhiteSpace(tableName)) return false;
+        if (values?.Any() ?? false) return false;
+        columns ??= new();
+        if (columns.Any() && columns.Count != values.Count) return false;
+
+        StringBuilder commandText = new("INSERT INTO");
+
+        commandText.Append(' ')
+                   .Append(tableName);
+
+        if (columns.Any())
         {
-            Console.WriteLine(ex);
-            return null;
+            commandText.Append(' ')
+                       .Append('(')
+                       .Append(string.Join(", ", columns))
+                       .Append(')');
         }
+
+        commandText.Append(' ')
+                   .Append("VALUES")
+                   .Append(' ')
+                   .Append('(')
+                   .Append(string.Join(", ", values))
+                   .Append(')')
+                   .Append(';');
+
+        return ExecuteVoidQuery(commandText.ToString());
+    }
+
+    public bool Update(string tableName, List<string> columns = null, List<string> values = null, List<(string column, string value, WhereClauseComparison compare, WhereClauseCombiner combine)> where = null, string customWhere = "")
+    {
+        if (string.IsNullOrWhiteSpace(tableName)) return false;
+        if (values?.Any() ?? false) return false;
+        if (columns?.Any() ?? false) return false;
+        if (columns.Any() && columns.Count != values.Count) return false;
+
+        StringBuilder commandText = new("UPDATE");
+
+        commandText.Append(' ')
+                   .Append(tableName);
+
+        commandText.Append(' ')
+                   .Append("SET")
+                   .Append(' ');
+
+        for (int i = 0; i < columns.Count; i++)
+        {
+            commandText.Append(string.Format("{0} = {1}", columns[i], values[i]));
+        }
+                   
+        if (where.Any())
+        {
+            commandText.Append(ConstructWhere(where));
+        }
+        else if (!string.IsNullOrWhiteSpace(customWhere))
+        {
+            commandText.Append(' ')
+                       .Append(customWhere);
+        }
+
+        commandText.Append(';');
+
+        return ExecuteVoidQuery(commandText.ToString());
+    }
+
+    public bool Delete(string tableName, List<(string column, string value, WhereClauseComparison compare, WhereClauseCombiner combine)> where = null, string customWhere = "")
+    {
+        if (string.IsNullOrWhiteSpace(tableName)) return false;
+
+        where ??= new();
+
+        StringBuilder commandText = new("DELETE FROM");
+
+        commandText.Append(' ')
+                   .Append(tableName);
+
+        if (where.Any())
+        {
+            commandText.Append(ConstructWhere(where));
+        }
+        else if (!string.IsNullOrWhiteSpace(customWhere))
+        {
+            commandText.Append(' ')
+                       .Append(customWhere);
+        }
+        commandText.Append(';');
+
+        return ExecuteVoidQuery(commandText.ToString());
+    }
+
+    private string ConstructWhere(List<(string column, string value, WhereClauseComparison compare, WhereClauseCombiner combine)> where)
+    {
+        StringBuilder whereBuilder = new StringBuilder();
+        for (int i = 0; i < where.Count; i++)
+        {
+            if (i == 0)
+            {
+                whereBuilder.Append(' ')
+                           .Append("WHERE")
+                           .Append(' ');
+            }
+            (string column, string value, WhereClauseComparison compare, WhereClauseCombiner combine) = where[i];
+
+            whereBuilder.Append(column)
+                       .Append(' ');
+
+            whereBuilder.Append(compare switch
+            {
+                WhereClauseComparison.Unknown => throw new NotImplementedException("Where clause cannot be Unknown!"),
+                WhereClauseComparison.Equals => '=',
+                WhereClauseComparison.NotEquals => "<>",
+                WhereClauseComparison.Greater => '>',
+                WhereClauseComparison.Less => '<',
+                WhereClauseComparison.GreaterOrEqual => "<=",
+                WhereClauseComparison.LessOrEqual => ">=",
+                _ => throw new NotImplementedException("How even??"),
+            });
+
+            whereBuilder.Append(' ')
+                       .Append(value)
+                       .Append(' ');
+
+            if (i < where.Count - 1 && combine == WhereClauseCombiner.None) combine = WhereClauseCombiner.And;
+
+            whereBuilder.Append(combine switch
+            {
+                WhereClauseCombiner.None => "",
+                WhereClauseCombiner.And => "AND",
+                WhereClauseCombiner.Or => "OR",
+                _ => throw new NotImplementedException("How even??"),
+            });
+
+            whereBuilder.Append(' ');
+        }
+
+        return whereBuilder.ToString();
     }
 
     protected virtual void Dispose(Boolean disposing)
